ff7a529d90e14f130c088dbea4ddb45258a712d2
GenaPlem <shainkill1337@gmail.com>
2023-10-23 17:48:33 +0100

    Help!!

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -18,7 +18,7 @@ if os.path.isfile('env.py'):
     import env
 
 
-development = 'DEVELOPMENT' in os.environ
+development = bool(os.environ.get('DEVELOPMENT'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
