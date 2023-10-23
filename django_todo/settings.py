337ac75a96b43e95689d7bf0e268e5154eea4b33
GenaPlem <shainkill1337@gmail.com>
2023-10-23 16:56:56 +0100

    Fix develompent mode

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -17,7 +17,8 @@ import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
-development = os.environ.get('DEVELOPMENT', False)
+
+development = False or os.environ.get('DEVELOPMENT')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
