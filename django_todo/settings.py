a0c537a5b99df9ffe318c5345b770b944dd59ff0
GenaPlem <shainkill1337@gmail.com>
2023-10-23 15:41:56 +0100

    Fix vars in settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-nwuhpx8irpgrkyg47-=8f2tztnb=3*f$xgm0p$rial%it3xkke')
+SECRET_KEY = 'django-insecure-nwuhpx8irpgrkyg47-=8f2tztnb=3*f$xgm0p$rial%it3xkke'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+ALLOWED_HOSTS = []
 
 
 # Application definition
